87312390672939b2bf07eb6e56443e3acd649ef3
Lifta-520 <133032870+Lifta-520@users.noreply.github.com>
2023-06-17 20:47:14 +0700

    Add files via upload

---
+++ B2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+void Dichuyen(int n, char from, char to, char trunggian) {
+    if (n == 1) {
+        std::cout << "Di chuyen dia 1 tu " << from << " den " << to << std::endl;
+        return;
+    }
+
+    Dichuyen(n - 1, from, trunggian, to);
+    std::cout << "Di chuyen dia " << n << " tu " << from << " den " << to << std::endl;
+    Dichuyen(n - 1, trunggian, from , to);
+}
+
+void thapHN(int n) {
+    Dichuyen(n, 'A', 'C', 'B');
+}
+
+int main() {
+    int soLuongDia;
+    std::cout << "Nhap so luong dia: ";
+    std::cin >> soLuongDia;
+
+    thapHN(soLuongDia);
+
+    return 0;
+}
